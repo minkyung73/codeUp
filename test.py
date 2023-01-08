b8461d07be06389d4e7b2e87cdc0e8fde5d574a1
minkyung <minkyung7365@gmail.com>
2023-01-08 22:13:34 +0900

    230108

--- test.py
+++ test.py
@@ -1,13 +1,5 @@
-cnt=0
 
-def f(x):
-    if x<1: return
-    print(x, "*")
-    f(x//2)
-    print(x%2, end='')
+str = "helloworld"
 
-
-f(10)
-print()
-f(13)
-# print(cnt)
+for i in range(str):
+    print(i)
