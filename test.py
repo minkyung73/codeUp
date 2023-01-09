f1867fcfc751b401cbb9542f626b66be4153cfed
minkyung <minkyung7365@gmail.com>
2023-01-09 21:39:49 +0900

    230109

--- test.py
+++ test.py
@@ -1,5 +1,17 @@
+def solution(rsp):
+    answer = ''
+    
+    for i in range(len(rsp)):
+        if(rsp[i]==2):
+            answer += '0'
+            print(i, answer)
+        elif(rsp[i]==0):
+            answer += '5'
+            print(i, answer)
+        elif(rsp[i]==5):
+            answer += '2'
+            print(i, answer)
+    
+    return answer
 
-str = "helloworld"
-
-for i in range(str):
-    print(i)
+print(solution("205"))
