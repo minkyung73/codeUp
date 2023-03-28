ec78b5d6c47af763bd6ffe1cbe5cb16dca808592
minkyung <minkyung7365@gmail.com>
2023-03-28 22:23:34 +0900

    재귀

--- python/recursion/1905.py
+++ python/recursion/1905.py
@@ -1,2 +1,17 @@
 from sys import stdin
+import sys
 
+sys.setrecursionlimit(10**7)
+
+def f(n):
+    global s
+    if n < 1:
+        return s
+    s += n
+    f(n-1)
+
+
+s = 0
+n = int(stdin.readline())
+f(n)
+print(s)
