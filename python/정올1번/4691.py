fd3ca964d80f97e94fe0e43a80af4d1028104555
minkyung <minkyung7365@gmail.com>
2023-04-27 17:24:43 +0900

    주사위 네 개

--- python/정올1번/4691.py
+++ python/정올1번/4691.py
@@ -4,12 +4,56 @@ n = int(stdin.readline().strip())
 dice = []
 for _ in range(n):
     dice.append(list(map(int, stdin.readline().strip().split())))
+# print(dice)
 
-price = [0] * n
+dice_num = []
 for i in range(n):
-    if dice[i][0] == dice[i][1] == dice[i][2] == dice[i][3]:
-        price.append(50000 + dice[i][0] * 5000)
+    temp = [0] * 6
+    for j in range(4):
+        temp[dice[i][j]-1] += 1
+    dice_num.append(temp)
+# print(dice_num)
+
+price = []
+
+for i in range(n):
+    # 4개의 눈이 모두 다른 경우
+    cnt = 0
+    flag1 = 0
+    for j in range(6):
+        if dice_num[i][j] == 1:
+            cnt += 1
+        if cnt == 4:
+            flag1 = 1
+            price.append(max(dice[i]) * 100)
+            break
+    if flag1 == 1:
         continue
 
-    count = 0
+    # 4, 3, 2
+    for j in range(6):
+        if dice_num[i][j] == 4:
+            price.append(50000 + (j+1)*5000)
+            break
+        elif dice_num[i][j] == 3:
+            price.append(10000 + (j+1)*1000)
+            break
+        elif dice_num[i][j] == 2:
+            flag = 0
+            for k in range(j+1, 6):
+                if dice_num[i][k] == 2:
+                    flag = 1
+                    price.append(2000 + (j+1) * 500 + (k+1) * 500)
+                    break
+            if flag == 1:
+                break
+            else:
+                price.append(1000 + (j+1) * 100)
+                break
+
+# print(price)
+print(max(price))
+
+
+
 
--- test.c
+++ test.c
@@ -1,28 +1,12 @@
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>
-# define N 5
 
-int main(void){
-    int p[11][11];
-    int i, j;
-    int n=8, r=5;
-
-    for(i=0;i<=n;i++){
-        for(j=0;j<=n;j++){
-            p[i][j] = 0;
-        }
-    }
-
-    p[0][0] = 1;
-
-    for(i=0;i<=n;i++){
-        p[i][0] = 1;
-
-        for(j=1;j<=i;j++) p[i][j] = p[i-1][j-1] + p[i-1][j];
-    }
-
-    printf("%d ", p[n][r]);
-    return p[n][r];
-
+int main()
+{
+    char s1[30] = "A Garden Diary";
+    char *ptr = strchr(s1, 'a');
+    while(ptr != NULL){
+        printf("%s\n", ptr);
+        ptr = strchr(ptr + 1, 'a');
+    }   
 }
